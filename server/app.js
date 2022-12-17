274ac891a3bd8e01e8a0537eecb2a8185a75859d
Miri <mirifurman@gmail.com>
2022-12-18 00:40:05 +0200

    front connected

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import cors from 'cors'
 import { indexRouter } from './routes/index.router.js'
+import * as url from 'url'
+import path from 'path'
 
-const app=express()
+const __dirname = url.fileURLToPath(new URL('./', import.meta.url))
+
+const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use('/api',indexRouter)
+const publicPath = path.join(__dirname, 'build')
 
-export {app}
+app.use(express.static(publicPath))
+app.use('/api', indexRouter)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'build','index.html'))
+})
+export { app }

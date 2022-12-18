83d5862906f4e2b552cca0af37530d36f897c660
Miri <mirifurman@gmail.com>
2022-12-18 23:26:15 +0200

    start od react

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,67 @@ import './App.css';
 import { useState, useEffect } from 'react'
 import { Api } from './api/api.js'
 
+
 function App() {
-  const [plants, setPlants] = useState(null)
+  const [accounts, setAccounts] = useState(null)
+  const [showAccounts, setShowAccounts] = useState(false)
 
   useEffect(() => {
-    Api.get('/plants').then(({data}) => {
-      setPlants(data)
+    Api.get('/accounts').then(({ data }) => {
+      setAccounts(data)
       console.log(data);
     }).catch(e => console.log(e))
-  })
+  }, [])
+
+  const showUsers = () => {
+    setShowAccounts(prev => !prev)
+  }
+  const deleteAccount = (e) => {
+    let id=e.target.className
+    console.log(id);
+    try {
+      Api.delete(`/accounts/${id}/delete`)
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  
+  const addAccount=()=>{
+
+  }
   return (
     <div className="App">
-     <h1>hello</h1>
+      <h1>hello</h1>
+      <button onClick={showUsers}>see all users</button>
+      <button onClick={addAccount}> Add new account</button>
+      <button> Deposit money</button>
+      <button> withdraw money</button>
+      <button> Transfer money</button>
+      <form>
+        <label>user name</label>
+        <input></input>
+        <label>user id</label>
+        <input></input>
+        <label>user cash</label>
+        <input></input>
+        <label>user credit</label>
+        <input></input>
+        <button>add user</button>
+        
+        
+      </form>
+      {showAccounts && accounts.map((account) => {
+        return (<div key={account.userId}>
+          <h4>Name: {account.name}</h4>
+          <h5>Id: {account.userId}</h5>
+          <h5>Cash: {account.cash}</h5>
+          <h5>Credit: {account.credit}</h5>
+          <button onClick={deleteAccount} className={account._id}>delete</button>
+        </div>
+        ) 
+      })}
+
     </div>
   );
 }
